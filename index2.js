8bddb44cd73d1ac270d81c0756247a176c05716e
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-01-22 16:43:07 +0100

    encrypt then auth

--- index2.js
+++ index2.js
@@ -1,69 +1,169 @@
-var fs = require('fs')
+/*
+  TODO:
+    + encrypt the connection using ecdhe before exchanging any credentials !!!
+    + move from elliptic to builtin crypto ECDH
+*/
 
-// forget the type def, just make it a closure factory
-// and no public blacklisting either that is controlled at init
+var crypto = require('crypto')
+var net = require('net')
+var pumpify = require('pumpify')
+var EC = require('elliptic').ec
 
-function isString (x) {
-  return typeof x === 'string' || x instanceof String
+var ec = new EC('curve25519')
+
+function sha512 (buf) {
+  return crypto.createHash('sha512').update(buf).digest()
 }
 
 function where (socket) {
   return socket.remoteAddress + ':' + socket.remotePort
 }
 
-// allow: String | Buffer | String[]
-//   schema:: [ sha512(user + ':' + password), ... ]
-// onauth: Function
-// opts.that: this
-// opts.maxTries: 3
-function NetGate (allow, opts, onauth) {
-  if (!(this instanceof NetGate)) return new NetGate(allow, opts, onauth)
+function isValid (allow, suspect) {
+  suspect = Buffer.isBuffer(suspect) ? suspect : Buffer.from(suspect, 'hex')
+  return allow.some(function (hash) {
+    return hash.equals(suspect)
+  })
+}
 
+/*
+  opts.cipherAlgorithm: string = aes192
+  opts.maxTries: number = 3
+*/
+function netGate (allow, opts, onauth) {
   if (typeof opts === 'function') {
     onauth = opts
     opts = {}
   }
 
-  if (!allow || !onauth) throw Error('allow \'am')
+  if (!allow || !onauth) throw Error('missing arguments')
 
-  if (isString(allow) || Buffer.isBuffer(allow)) allow = JSON.parse(allow)
+  if (!Array.isArray(allow)) allow = JSON.parse(allow)
   if (!opts) opts = {}
 
-  this._opts = opts
-  this._onauth = onauth
-  this._invalids = {}
-  this._blacklist = []
-  this._allow = allow.map(function (hash) {
+  opts.maxTries = opts.maxTries || 3
+  opts.cipherAlgorithm = opts.cipherAlgorithm || 'aes256'
+
+  var invalids = {}
+  var blacklist = []
+  var allow = allow.map(function (hash) {
     return Buffer.isBuffer(hash) ? hash : Buffer.from(hash, 'hex')
   })
 
+  function gate (socket) {
+    var loc = where(socket)
+    if (socket.bytesRead || blacklist.includes(loc)) return
+
+    var keypair = ec.genKeyPair()
+    var pubkey = Buffer.from(keypair.getPublic('binary'))
+    var encrypt
+    var decrypt
+    var nonce
+    var upw = Buffer.alloc(0)
+
+    var numReadables = 0
+
+    socket.on('readable', function readable () {
+      // callback counter
+      ++numReadables
+      if (numReadables === 1) {
+        // sending server's pubkey
+        socket.write(pubkey)
+        // gettin client's pubkey
+        var otherPubkey = socket.read(32)
+        // computing the shared secret
+        var shared = keypair.derive(ec.keyFromPublic(otherPubkey).getPublic())
+        // hashing the shared secret to a nonce 4 use with symmetric encryption
+        nonce = sha512(shared.toString(16))
+console.log('SERVER NONCE', nonce)
+        // binding en/decryption to the socket
+        encrypt = crypto.createCipher(opts.cipherAlgorithm, nonce)
+        decrypt = crypto.createDecipher(opts.cipherAlgorithm, nonce)
+        // socket = pumpify(encrypt, socket, decrypt)
+
+      } else if (numReadables === 2) {
+        // expecting: sha512(user + ':' + password)
+        var suspect = socket.read(64)
+console.log('SUSPECT', suspect, suspect.length)
+        var decrypt2 = crypto.createDecipher(opts.cipherAlgorithm, nonce)
+        decrypt2.setAutoPadding(false)
+
+
+
+        decrypt2.on('data', function (chunk) {
+console.log('CHUNK', chunk, chunk.length)
+          upw = Buffer.concat([ upw, chunk ])
+        })
+
+        decrypt2.on('end', function () {
+console.log('DECRYPTED UPW', upw, upw.length)
+          if (!isValid(allow, upw)) {
+            if (invalids.hasOwnProperty(loc)) invalids[loc]++
+            else invalids[loc] = 1
+            if (invalids[loc] >= opts.maxTries) blacklist.push(loc)
+          } else {
+console.log('AUTHENTICATED!!!!!!!')
+            onauth(pumpify(encrypt, socket, decrypt))
+          }
+
+          // once we got pubkey and upw forget this handler
+          socket.removeListener('readable', readable)
+        })
+
+        decrypt2.write(suspect)
+        decrypt2.end()
+
+      }
+
+     // now only left with getting auth data thru
+
+
+
+    })
+
+    // socket.once('readable', function () {
+    //
+    //   var suspect = socket.read(64)  // expecting: sha512(user + ':' + password)
+    //
+    //
+    //
+    //   if (!isValid(allow, suspect)) {
+    //     if (invalids.hasOwnProperty(loc)) invalids[loc]++
+    //     else invalids[loc] = 1
+    //     if (invalids[loc] >= opts.maxTries) blacklist.push(loc)
+    //   } else {
+    //     onauth(socket)
+    //   }
+    // })
+  }
+
+  return gate
 }
 
-NetGate.prototype._isValid = function isValid (suspect) {
-  suspect = Buffer.isBuffer(suspect) ? suspect : Buffer.from(suspect, 'hex')
-  return this._allow.some(function (hash) {
-    return hash.equals(suspect)
+function clientEstablish (socket, upw, opts) {
+  if (!opts) opts = {}
+  opts.cipherAlgorithm = opts.cipherAlgorithm || 'aes256'
+  // ECDHE keys
+  var keypair = ec.genKeyPair()
+  var pubkey = Buffer.from(keypair.getPublic('binary'))
+  // sending clilent's pubkey
+  socket.write(pubkey)
+  // gettin servert's pubkey
+  socket.once('readable', function () {
+    var otherPubkey = socket.read(32)
+    // computing the shared secret
+    var shared = keypair.derive(ec.keyFromPublic(otherPubkey).getPublic())
+    // hashing the shared secret to a nonce 4 use with symmetric encryption
+    var nonce = sha512(shared.toString(16))
+console.log('CLIENT NONCE', nonce)
+    // binding en/decryption to the socket
+    var encrypt = crypto.createCipher(opts.cipherAlgorithm, nonce)
+    var decrypt = crypto.createDecipher(opts.cipherAlgorithm, nonce)
+    socket = pumpify(encrypt, socket, decrypt)
+    // authentication
+console.log('UPW', upw, upw.length)
+    socket.write(upw)
   })
 }
 
-// designed to be the very first connection handler of a net.Server instance
-// onauth(socket) callback is only called if the socket could be authenticated
-NetGate.prototype.protect = function protect (socket) {
-    var loc = where(socket)
-
-    if (socket.bytesRead || this._blacklist.includes(loc)) return
-
-    var suspect = socket.read(64)  // expecting: sha512(user + ':' + password)
-
-    if (!suspect) return
-    if (!this._isValid(suspect)) {
-      if (this._invalids.hasOwnProperty(loc)) this._invalids[loc]++
-      else this._invalids[loc] = 1
-      if (this._invalids[loc] >= this._opts.maxTries) this._blacklist.push(loc)
-      return
-    }
-
-    this._onauth(socket)
-  }
-
-module.exports = NetGate
+module.exports = { createGate: netGate, clientEstablish: clientEstablish }
--- test.js
+++ test.js
@@ -1,29 +1,58 @@
 var tape = require('tape')
 var net = require('net')
 var crypto = require('crypto')
+var EC = require('elliptic').ec
+var ec = new EC('curve25519')
 
-var NetGate = require('./index2')
+var netGate = require('./index')
+var ellGate = require('./index2').createGate
+var clientEstablish = require('./index2').clientEstablish
 
 function sha512 (buf) {
   return crypto.createHash('sha512').update(buf).digest()
 }
 
-tape('auth', function (t) {
+tape('unencrypted authentication', function (t) {
+
+  var cred = sha512('chiefbiiko:fraud')
+
+  var gate = netGate([ cred ], onauth)
+  var server = net.createServer(gate)
+  var client
 
   function onauth (socket) {
     t.true(true, 'authenticated successfully')
+    client.destroy()
     server.close()
     t.end()
   }
 
+  server.listen(10000, 'localhost', function () {
+    client = net.connect(10000, 'localhost', function () {
+      client.write(cred)
+    })
+  })
+
+})
+
+tape('encrypted authentication', function (t) {
+
   var cred = sha512('chiefbiiko:fraud')
 
-  var gate = NetGate([ cred ], onauth)
-  var server = net.createServer(gate.protect.bind(gate))
+  var gate = ellGate([ cred ], onauth)
+  var server = net.createServer(gate)
+
+  function onauth (socket) {
+    t.true(true, 'authenticated successfully')
+    socket.end()
+    socket.destroy()
+    server.close()
+    t.end()
+  }
 
   server.listen(10000, 'localhost', function () {
     var client = net.connect(10000, 'localhost', function () {
-      client.write(cred)
+      clientEstablish(client, cred)
     })
   })
 
